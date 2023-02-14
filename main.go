7b9ec8d4171782db8885522b08fb677cecd5eb29
My-Ink <dev.myink@gmail.com>
2023-02-14 15:06:43 +0300

    rename solution.go

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package golang
 
-/*
+import (
+    "sort"
+)
+
 func GroupAndSort(users []UserData) map[string][]UserData {
-    panic("not implemented") // your code here
+    grouped := make(map[string][]UserData, len(users))
+    for _, user := range users {
+        grouped[user.City] = append(grouped[user.City], user)
+    }
+    for _, citizens := range grouped {
+        sort.Slice(citizens, func(i, j int) bool {
+            return citizens[i].ID < citizens[j].ID
+        })
+    }
+    return grouped
 }
-*/

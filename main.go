c6f9c71759d9ae58d5ec939bdaddd107193588d8
Aleksandr Kurov <a.kurov@gbc-team.com>
2023-02-22 21:09:25 +0300

    hw1 solved

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	// implement me
+	// fmt.Println(strings.Join(os.Args[1:], " ")) one-liner
+
+	res := make([]any, len(os.Args)-1)
+
+	for i, v := range os.Args[1:] {
+		res[i] = strings.TrimSpace(v)
+	}
+
+	fmt.Println(res...)
 }

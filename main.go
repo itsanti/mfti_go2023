4e2f42b561e5dc762ee86704c54a50bd97a21cad
Aleksandr Kurov <a.kurov@gbc-team.com>
2023-02-27 20:43:19 +0300

    hw3 solved

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import "sort"
+
+// how to sort in go https://stackoverflow.com/a/40932847
+
 func GroupAndSort(users []UserData) map[string][]UserData {
-	// implement me
-	return nil
+	var res = make(map[string][]UserData, len(users))
+	if len(users) == 0 {
+		return res
+	}
+	for _, user := range users {
+		res[user.City] = append(res[user.City], user)
+	}
+	for _, userData := range res {
+		userDataSort(userData)
+	}
+	return res
+}
+
+func userDataSort(userData []UserData) {
+	sort.Slice(userData, func(i, j int) bool {
+		return userData[i].ID < userData[j].ID
+	})
 }

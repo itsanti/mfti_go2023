b0a76a942723273f8aff200c873f69693f0f530e
Aleksandr Kurov <a.kurov@gbc-team.com>
2023-03-19 21:57:07 +0300

    Code review 1

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 	// fmt.Println(strings.Join(os.Args[1:], " ")) one-liner
 
-	res := make([]any, len(os.Args)-1)
+	res := make([]string, len(os.Args)-1)
 
 	for i, v := range os.Args[1:] {
 		res[i] = strings.TrimSpace(v)
 	}
 
-	fmt.Println(res...)
+	fmt.Println(strings.Join(res, " "))
 }

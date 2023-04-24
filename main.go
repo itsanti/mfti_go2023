ad7f53c870520df4fd5cfd7be7ad8222f9de2ae4
Aleksandr Kurov <a.kurov@gbc-team.com>
2023-04-24 19:26:29 +0300

    Review fix

--- main.go
+++ main.go
@@ -10,22 +10,22 @@ type APIclient interface {
 }
 
 func GetAPIclient(mock bool) APIclient {
-	var a APIclient
+	var client APIclient
 
 	if mock {
-		a = MockingHTTPclient{Count: 3}
+		client = MockingHTTPclient{Count: 3}
 	} else {
-		const timeout = 2 * time.Second
-		const retries = 3
-		const debug = false
-		a = RetryableHTTPclient{Timeout: timeout, Retries: retries, Debug: debug}
+		client = RetryableHTTPclient{
+			Timeout: 2 * time.Second,
+			Retries: 3,
+			Debug:   false,
+		}
 	}
-	return a
+	return client
 }
 
 func main() {
-	var mocking bool
-	mocking = true
+	mocking := true
 	pokemons := GetAPIclient(mocking).GetPokemons()
 	for _, pokemon := range pokemons {
 		fmt.Println(pokemon)
